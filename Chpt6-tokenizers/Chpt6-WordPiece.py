d9b24d1e3a293fdda45ebb2f7800c14a8ed7031e
fzhu0628 <fzhu0628@gmail.com>
2026-04-20 15:50:59 -0400

    new file:   Chpt6-tokenizers/Chapt6-Building-a-tokenizer-block-by-block 	new file:   Chpt6-tokenizers/Chapt6-Unigram.py 	new file:   Chpt6-tokenizers/Chpt6-WordPiece.py

---
+++ Chpt6-tokenizers/Chpt6-WordPiece.py
@@ -0,0 +1,141 @@
+'''WordPiece starts from a small vocabulary including the special tokens used by the model and the initial alphabet.'''
+
+'''WordPiece defines subwords by adding a prefix except for the first subword,
+ which is used to indicate that the subword is a continuation of a previous subword.'''
+
+'''The initial aphabet contains all the characters present at the BEGINNNING of a word,
+and the characters present inside a word preceded by the WordPiece prefix.'''
+
+'''Example: "word" gets split into w ##o ##r ##d'''
+
+'''score=(freq_of_pair)/(freq_of_first_element×freq_of_second_element)
+Merge pairs with less individual frequency'''
+
+'''WordPiece only saves the final vocabulary, not the merge rules learned
+Starting from the word to tokenize, WordPiece finds the longest subword that is in the vocabulary, 
+then splits on it.'''
+
+'''When the tokenization gets to a stage where it’s not possible to find a subword in the vocabulary,
+ the whole word is tokenized as unknown'''
+
+corpus = [
+    "This is the Hugging Face Course.",
+    "This chapter is about tokenization.",
+    "This section shows several tokenizer algorithms.",
+    "Hopefully, you will be able to understand how they are trained and generate tokens.",
+]
+
+from transformers import AutoTokenizer
+
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased") # pretokenize
+
+from collections import defaultdict
+
+word_freqs = defaultdict(int)
+for text in corpus:
+    words_with_offsets = tokenizer.backend_tokenizer.pre_tokenizer.pre_tokenize_str(text)
+    new_words = [word for word, offset in words_with_offsets]
+    for word in new_words:
+        word_freqs[word] += 1 # compute the frequency of each word in the corpus
+
+alphabet = []
+for word in word_freqs.keys():
+    if word[0] not in alphabet:
+        alphabet.append(word[0])
+    for letter in word[1:]:
+        if f"##{letter}" not in alphabet:
+            alphabet.append(f"##{letter}")
+
+alphabet.sort()
+print(alphabet)
+
+vocab = ["[PAD]", "[UNK]", "[CLS]", "[SEP]", "[MASK]"] + alphabet.copy()
+
+splits = {
+    word: [c if i == 0 else f"##{c}" for i, c in enumerate(word)]
+    for word in word_freqs.keys()
+} # split each word into characters
+
+def compute_pair_scores(splits):
+    letter_freqs = defaultdict(int)
+    pair_freqs = defaultdict(int)
+    for word, freq in word_freqs.items():
+        split = splits[word]
+        if len(split) == 1:
+            letter_freqs[split[0]] += freq
+            continue
+        for i in range(len(split) - 1):
+            pair = (split[i], split[i + 1])
+            letter_freqs[split[i]] += freq
+            pair_freqs[pair] += freq
+        letter_freqs[split[-1]] += freq
+
+    scores = {
+        pair: freq / (letter_freqs[pair[0]] * letter_freqs[pair[1]])
+        for pair, freq in pair_freqs.items()
+    }
+    return scores
+
+pair_scores = compute_pair_scores(splits)
+
+best_pair = ""
+max_score = None
+for pair, score in pair_scores.items():
+    if max_score is None or max_score < score:
+        best_pair = pair
+        max_score = score
+
+print(best_pair, max_score)
+
+def merge_pair(a, b, splits):
+    for word in word_freqs:
+        split = splits[word]
+        if len(split) == 1:
+            continue
+        i = 0
+        while i < len(split) - 1:
+            if split[i] == a and split[i + 1] == b:
+                merge = a + b[2:] if b.startswith("##") else a + b
+                split = split[:i] + [merge] + split[i + 2 :]
+            else:
+                i += 1
+        splits[word] = split
+    return splits
+
+# Now we can merge the best pair and add it to the vocabulary:
+
+vocab_size = 70
+while len(vocab) < vocab_size:
+    scores = compute_pair_scores(splits)
+    best_pair, max_score = "", None
+    for pair, score in scores.items():
+        if max_score is None or max_score < score:
+            best_pair = pair
+            max_score = score
+    splits = merge_pair(*best_pair, splits)
+    new_token = (
+        best_pair[0] + best_pair[1][2:]
+        if best_pair[1].startswith("##")
+        else best_pair[0] + best_pair[1]
+    )
+    vocab.append(new_token)
+
+def encode_word(word): # tokenize a word using the vocabulary
+    tokens = []
+    while len(word) > 0:
+        i = len(word)
+        while i > 0 and word[:i] not in vocab:
+            i -= 1
+        if i == 0:
+            return ["[UNK]"]
+        tokens.append(word[:i])
+        word = word[i:]
+        if len(word) > 0:
+            word = f"##{word}"
+    return tokens
+
+def tokenize(text):
+    pre_tokenize_result = tokenizer._tokenizer.pre_tokenizer.pre_tokenize_str(text)
+    pre_tokenized_text = [word for word, offset in pre_tokenize_result]
+    encoded_words = [encode_word(word) for word in pre_tokenized_text]
+    return sum(encoded_words, [])
